974c094afbbb8f4419fb7a097b0edc5380945b20
Isaac Leong <isaacleong23367@gmail.com>
2025-11-19 16:45:03 +0800

    Added Parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+
+/// Weekly commitment tracker
+#[derive(Parser)]
+#[command(
+    name = "Weekly Progress Tracker",
+    about = "Track weekly commitments and hours",
+    version = "0.0.1",
+    author = "Isaac Leong"
+)]
+#[derive(Debug)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Add { name: String, weekly_hours: f32 },
+    Remove { id: u32 },
+    Log { id: u32, hours: f32 },
+    List,
+    History { id: u32 },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("{:#?}", cli);
 }
